d9e1cc79f051e321f33453c7f80b396f32457af1
mysicks <alo.madri@gmail.com>
2023-01-23 20:29:23 -0600

    adding flask

---
+++ src/Components/InteractiveFrame.js
@@ -0,0 +1,33 @@
+import * as React from 'react';
+import CameraFrame from './CameraFrame';
+import TextComponent from './TextComponent';
+import Button from '@mui/material/Button';
+import CVCamera from 'CVCamera.js';
+
+function InteractiveFrame(props) {
+  const [isPaused, setPaused] = React.useState(false);
+
+  const handlePause = () => {
+    setPaused(!isPaused);
+  };
+
+  return (
+    <div>
+      <div class='flex justify-evenly w-screen'>
+        <CVCamera />
+        <TextComponent />
+      </div>
+
+      <div class='absolute inset-x-0 bottom-0 h-16'>
+        <Button variant='contained' type='button' onClick={props.handleClick}>
+          End
+        </Button>
+        <Button variant='Text' type='button' onClick={handlePause}>
+          {isPaused ? 'Resume' : 'Pause'}
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default InteractiveFrame;
