fc6bab22975b71d451a5dc0f74c18a783b5b76d4
mysicks <alo.madri@gmail.com>
2023-04-24 21:12:10 -0600

    nueva ui

--- frontend/src/Components/CVCamera.js
+++ frontend/src/Components/CVCamera.js
@@ -1,11 +1,17 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 const Cam = (props) => {
   return (
-    <div class='box-content h-96 w-96 rounded-lg'>
+    <div class="h-96 w-3/5 ">
       {props.isPaused ? (
-        <div>Video Paused</div>
+        <div className="flex justify-end items-center h-96 w-96 font-mono text-2xl ">
+          Video Pausado
+        </div>
       ) : (
-        <img src='http://localhost:5000/api/video_feed' alt='Video' />
+        <img
+          className="rounded-2xl drop-shadow-2xl"
+          src="http://localhost:5000/api/video_feed"
+          alt="Video"
+        />
       )}
     </div>
   );
--- frontend/src/Components/InteractiveFrame.js
+++ frontend/src/Components/InteractiveFrame.js
@@ -1,8 +1,8 @@
-import React, { useState, useEffect } from 'react';
-import Button from '@mui/material/Button';
-import CVCamera from './CVCamera.js';
-import { io } from 'socket.io-client';
-import Box from '@mui/material/Box';
+import React, { useState, useEffect } from "react";
+import Button from "@mui/material/Button";
+import CVCamera from "./CVCamera.js";
+import { io } from "socket.io-client";
+import Box from "@mui/material/Box";
 
 function InteractiveFrame(props) {
   const [isPaused, setPaused] = useState(false);
@@ -13,17 +13,17 @@ function InteractiveFrame(props) {
   };
 
   useEffect(() => {
-    const socket = io('localhost:5000/', {
-      transports: ['polling'],
+    const socket = io("localhost:5000/", {
+      transports: ["polling"],
       cors: {
-        origin: 'http://localhost:3000/',
+        origin: "http://localhost:3000/",
       },
     });
 
     if (isPaused === false) {
-      socket.emit('translate');
+      socket.emit("translate");
 
-      socket.on('data', (data) => {
+      socket.on("data", (data) => {
         let newTexts = [...texts, data.data];
         setTexts(newTexts);
       });
@@ -32,27 +32,33 @@ function InteractiveFrame(props) {
         socket.disconnect();
       };
     } else {
-      socket.emit('paused');
+      socket.emit("paused");
     }
   }, [isPaused, texts]);
 
   return (
     <div>
-      <div class='flex justify-evenly w-screen'>
+      <div class="flex justify-evenly w-screen absolute right-0 top-32">
         <CVCamera isPaused={isPaused} />
-        <Box component='span' sx={{ display: 'block' }}>
-          <h1>Translated text</h1>
-          {texts.join(' ')}
-        </Box>
+        <div className="h-96 w-1/3 rounded-2xl">
+          <h1 className="font-bold text-2xl font-mono">Texto Traducido</h1>
+          {texts.join(" ")}
+        </div>
       </div>
 
-      <div class='absolute inset-x-0 bottom-0 h-16'>
-        <Button variant='contained' type='button' onClick={props.handleClick}>
-          End
-        </Button>
-        <Button variant='Text' type='button' onClick={handlePause}>
-          {isPaused ? 'Resume' : 'Pause'}
-        </Button>
+      <div class="absolute inset-x-0 bottom-5 h-16 flex justify-center items-center gap-5">
+        <button
+          className="btn btn-md btn-outline drop-shadow-md "
+          onClick={props.handleClick}
+        >
+          Finalizar
+        </button>
+        <button
+          className="btn btn-md btn-accent btn-outline drop-shadow-md "
+          onClick={handlePause}
+        >
+          {isPaused ? "Continuar" : "Pausar"}
+        </button>
       </div>
     </div>
   );
